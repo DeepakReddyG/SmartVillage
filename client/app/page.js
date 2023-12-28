044be86203da9b845956aae9ced47da2391b967a
Deepak Reddy Gathpa <deepakreddygathpa@gmail.com>
2023-12-28 06:36:38 +0530

    stable section two

--- client/app/page.js
+++ client/app/page.js
@@ -1,9 +1,105 @@
-import Image from 'next/image'
+import Image from 'next/image';
+
+import './globals.css';
+
+import SVR_Image_1 from './Assets/SVR_Image_1.png';
 
 export default function Home() {
   return (
-    <main className="container">
+      <div className="home-component">
+        <div className="home-container">
+            <div className="home-two">
+              <div className="home-two-in">
+                <div className="home-two-in-header">
+                  <h1>Smart Village Revolution at K L Deemed to be University: Steps towards Adarsh Gram</h1>
+                </div>
+                <div className="home-two-in-one">
+                  <div className="home-two-in-one-in">
+                    <div className="home-two-in-one-in-one">
+                      <div className="home-two-in-one-in-one-in">
+                        <p>Smart Village Revolution at K L Deemed to be University is a rural development programme broadly focusing upon the development in the villages which includes social development, cultural development and spread motivation among the people on social mobilization of the village community. The programme was launched by the President of KLEF, Hon'ble Shri Koneru Lakshmaiah on the birth anniversary of Jayaprakash Narayan, on 11 October 2014.</p>
 
-    </main>
-  )
+                        <div className="home-two-in-one-in-boxes">
+                          <div className="home-two-in-one-in-boxes-in">
+                            <div className="home-two-in-one-in-boxes-in-box">
+                              <div className="home-two-in-one-in-boxes-in-box-in">
+                                <p>Smart Villare</p>
+                              </div>
+                            </div>
+
+                            <div className="home-two-in-one-in-boxes-in-box">
+                              <div className="home-two-in-one-in-boxes-in-box-in">
+                                <p>Sustainable Village</p>
+                              </div>
+                            </div>
+
+                            <div className="home-two-in-one-in-boxes-in-box">
+                              <div className="home-two-in-one-in-boxes-in-box-in">
+                                <p>Women Empowerment</p>
+                              </div>
+                            </div>
+
+                            <div className="home-two-in-one-in-boxes-in-box">
+                              <div className="home-two-in-one-in-boxes-in-box-in">
+                                <p>Social Impact</p>
+                              </div>
+                            </div>
+
+                            <div className="home-two-in-one-in-boxes-in-box">
+                              <div className="home-two-in-one-in-boxes-in-box-in">
+                                <p>Community</p>
+                              </div>
+                            </div>
+
+                            <div className="home-two-in-one-in-boxes-in-box">
+                              <div className="home-two-in-one-in-boxes-in-box-in">
+                                <p>Social Mobilization</p>
+                              </div>
+                            </div>
+
+                            <div className="home-two-in-one-in-boxes-in-box">
+                              <div className="home-two-in-one-in-boxes-in-box-in">
+                                <p>Cultural Development</p>
+                              </div>
+                            </div>
+
+                            <div className="home-two-in-one-in-boxes-in-box">
+                              <div className="home-two-in-one-in-boxes-in-box-in">
+                                <p>Women Empowerment</p>
+                              </div>
+                            </div>
+
+                            <div className="home-two-in-one-in-boxes-in-box">
+                              <div className="home-two-in-one-in-boxes-in-box-in">
+                                <p>Social Impact</p>
+                              </div>
+                            </div>
+
+                            <div className="home-two-in-one-in-boxes-in-box">
+                              <div className="home-two-in-one-in-boxes-in-box-in">
+                                <p>Community</p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="home-two-in-one-in-two">
+                      <div className="home-two-in-one-in-two-in">
+                       <Image
+                       className='home-two-in-one-in-two-in-image'
+                       src={SVR_Image_1}
+                        alt="Picture of the author"
+                        width={400}
+                        height={300}
+                       />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+        </div>
+      </div>
+    )
 }
