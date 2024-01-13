1bbbaff5fbeb088c93633cc3e66774d726c79d8f
saitadikonda99 <saitadikonda.tech@gmail.com>
2024-01-13 16:32:00 +0530

    vercel Analytics added

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,6 +15,7 @@ export default function RootLayout({ children }) {
       <html>
           <body className={inter.className}>
              {children}
+             <Analytics />
           </body>
     </html>
   )
