02417ea822653325e3ab7c1624caf4cecbdd2e70
Deepak Reddy Gathpa <deepakreddygathpa@gmail.com>
2024-08-20 22:37:00 +0530

    improvements in gallery

--- client/app/gallery/displayAsset.js
+++ client/app/gallery/displayAsset.js
@@ -1,50 +1,82 @@
-"use client"
+"use client";
 
 import React, { useEffect, useState } from "react";
-// import { ref, getDownloadURL } from "firebase/storage";
-import {ref, getDownloadURL} from "firebase/storage";
+import Image from "next/image";
+import { ref, getDownloadURL, getMetadata } from "firebase/storage";
 import { storage } from "../../firebase";
-
-import LoaderGif from '../../public/loader.gif';
+import loaderGif from "../../public/loader.gif";
 
 const MultiImageDisplay = ({ imagePaths }) => {
-  const [imageUrls, setImageUrls] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [images, setImages] = useState([]);
+  const [isAnyImageLoaded, setIsAnyImageLoaded] = useState(false);
 
   useEffect(() => {
-    const fetchImageUrls = async () => {
+    const fetchImagesWithMetadata = async () => {
       try {
-        const urls = await Promise.all(
+        const imageInfo = await Promise.all(
           imagePaths.map(async (path) => {
             const imageRef = ref(storage, path);
-            return await getDownloadURL(imageRef);
+            const url = await getDownloadURL(imageRef);
+            const metadata = await getMetadata(imageRef);
+            const date = new Date(metadata.timeCreated).toLocaleDateString();
+
+            return { url, date, loading: true };
           })
         );
-        setImageUrls(urls);
+        setImages(imageInfo);
       } catch (error) {
-        console.error("Error fetching image URLs:", error);
-      } finally {
-        setLoading(false);
+        console.error("Error fetching image URLs and metadata:", error);
       }
     };
 
-    fetchImageUrls();
+    fetchImagesWithMetadata();
   }, [imagePaths]);
+
+  const handleImageLoad = (index) => {
+    setImages((prevImages) =>
+      prevImages.map((img, i) =>
+        i === index ? { ...img, loading: false } : img
+      )
+    );
+    setIsAnyImageLoaded(true);
+  };
 
   return (
     <div className="gallery-three-prime">
-      {loading ? (
-        <img src={LoaderGif} alt="Loading" className="loader" />
-      ) : (
-        imageUrls.map((url, index) => (
+      {!isAnyImageLoaded && (
+        <div className="global-loader">
+          <Image
+            src={loaderGif}
+            alt="Loading"
+            width={100}
+            height={100}
+            style={{ objectFit: "cover", margin: "auto" }}
+          />
+        </div>
+      )}
+      {images.map((image, index) => (
+        <div key={index} className="gallery-item">
+          {image.loading ? (
+            <Image
+              className="gallery-image"
+              src={loaderGif}
+              alt="Loading"
+              width={50}
+              height={50}
+            />
+          ) : null}
           <img
-            key={index}
-            src={url}
+            src={image.url}
             alt={`Firebase Image ${index}`}
             className="gallery-image"
+            onLoad={() => handleImageLoad(index)}
+            style={image.loading ? { display: "none" } : {}}
           />
-        ))
-      )}
+          <p className="upload-date extra-small-text">
+            Uploaded on: {image.date}
+          </p>
+        </div>
+      ))}
     </div>
   );
 };
