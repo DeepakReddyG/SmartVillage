d1ea8d28096a820e915f0958cd10b006659ca622
Deepak Reddy Gathpa <deepakreddygathpa@gmail.com>
2024-08-20 12:43:45 +0530

    fire integrated - gallery page

---
+++ client/app/gallery/displayAsset.js
@@ -0,0 +1,46 @@
+"use client"
+
+import React, { useEffect, useState } from "react";
+import { ref, getDownloadURL } from "firebase/storage";
+import { storage } from "../../firebase";
+
+const MultiImageDisplay = ({ imagePaths }) => {
+  const [imageUrls, setImageUrls] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchImageUrls = async () => {
+      try {
+        const urls = await Promise.all(
+          imagePaths.map(async (path) => {
+            const imageRef = ref(storage, path);
+            return await getDownloadURL(imageRef);
+          })
+        );
+        setImageUrls(urls);
+      } catch (error) {
+        console.error("Error fetching image URLs:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchImageUrls();
+  }, [imagePaths]);
+
+  return (
+    <div>
+      {loading ? (
+        <p>Something went wrong, plse wait.</p>
+      ) : (
+        <div>
+          {imageUrls.map((url, index) => (
+            <img key={index} src={url} alt={`Firebase Image ${index}`} style={{ maxWidth: '50%', height: 'auto', margin: '10px', border: '2px solid black' }} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default MultiImageDisplay;
--- client/app/gallery/page.js
+++ client/app/gallery/page.js
@@ -1,93 +1,19 @@
-import React from 'react'
-import './page.css'
-import Link from 'next/link'
+import React from "react";
+import MultiImageDisplay from "./displayAsset";
 
+const App = () => {
+  const imagePaths = [
+    "gs://svrwebsite-1e892.appspot.com/gallery/Screenshot 2024-08-17 at 8.52.04 AM.png",
+    "gs://svrwebsite-1e892.appspot.com/gallery/svr_modifications.jpeg"
+  ];
 
-// import images here 
-
-import SVR_Image from '../Assets/SVR_Image.jpeg'
-import G20_Image from '../Assets/G20_Logo.png'
-import ElectionImage from '../Assets/Election.png'
-
-const page = () => {
   return (
-        <div className="GalleryComponent">
-            <div className="GalleryComponent-in">
-                <div className="Gallery-one">
-                    <div className="Gallery-one-in">
-                        <h1>Smart Village <span>Revolution</span> </h1>
-                        <Link href='/'>Back to Home</Link>
-                    </div>
-                </div>
-                <div className="Gallery-two">
-                    <div className="Gallery-two-in">
+    <div>
 
-                        <div className="Gallery-img-one gm-cm-one">
-                            <div className="Gallery-img-one-in gm-cm-one-in">
-                                <div className="Gi-one-one-in gm-cm-in-one g-img">
-                                    <img src={SVR_Image.src} alt="" />
-                                </div>
-                                <div className="Gi-one-two-in gm-cm-in-two g-txt">
-                                    <h1>Smart Village Revolution</h1>
-                                    <p>India is an mosaic of multicultural experiences. With a rich heritage and myriad attractions, the country is among the finest destinations for a holistic vacation.</p>
-                                </div>
-                            </div>
-                        </div>
+    <h1>SVR Gallery Page</h1>
+      <MultiImageDisplay imagePaths={imagePaths} />
+    </div>
+  );
+};
 
-
-                        <div className="Gallery-img-two gm-cm-one">
-                            <div className="Gallery-img-two-in gm-cm-one-in">
-                                <div className="Gi-two-one-in gm-cm-in-one g-txt">
-                                    <h1>Smart Village Revolution</h1>
-                                    <p>India is a mosaic of multicultural experiences. With a rich heritage and myriad attractions, the country is among the finest destinations for a holistic vacation.</p>
-                                </div>
-                                <div className="Gi-two-two-in gm-cm-in-two g-img">
-                                    <img src={ElectionImage.src} alt="" />
-                                </div>
-                            </div>
-                        </div>
-                         
-                        <div className="Gallery-img-three gm-cm-one">
-                            <div className="Gallery-img-three-in gm-cm-one-in">
-                                <div className="Gi-three-one-in gm-cm-in-one g-img">
-                                    <img src={SVR_Image.src} alt="" />
-                                </div>
-                                <div className="Gi-three-two-in gm-cm-in-two g-txt">
-                                    <h1>Smart Village Revolution</h1>
-                                    <p>India is a mosaic of multicultural experiences. With a rich heritage and myriad attractions, the country is among the finest destinations for a holistic vacation.</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="Gallery-img-four gm-cm-one">
-                            <div className="Gallery-img-four-in gm-cm-one-in">
-                                <div className="Gi-four-one-in gm-cm-in-one g-txt">
-                                    <h1>Smart Village Revolution</h1>
-                                    <p>India is a mosaic of multicultural experiences. With a rich heritage and myriad attractions, the country is among the finest destinations for a holistic vacation.</p>
-                                </div>
-                                <div className="Gi-four-two-in gm-cm-in-two g-img">
-                                    <img src={ElectionImage.src} alt="" />
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="Gallery-img-five gm-cm-one">
-                            <div className="Gallery-img-five-in gm-cm-one-in">
-                                <div className="Gi-five-one-in gm-cm-in-one g-img">
-                                    <img src={SVR_Image.src} alt="" />  
-                                </div>
-                                <div className="Gi-five-two-in gm-cm-in-two g-txt">
-                                    <h1>Smart Village Revolution</h1>
-                                    <p>India is a mosaic of multicultural experiences. With a rich heritage and myriad attractions, the country is among the finest destinations for a holistic vacation.</p>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-  )
-}
-
-export default page
+export default App;
